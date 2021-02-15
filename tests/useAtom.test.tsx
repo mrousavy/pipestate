85460ba217713bd7c22b11a70cce4a7f088e44fd
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-15 15:19:32 +0100

    Add react tests

--- tests/useAtom.test.tsx
+++ tests/useAtom.test.tsx
@@ -1,6 +1,27 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+import { renderHook, act } from '@testing-library/react-hooks/native'
 import { useAtom } from '../src'
 import { atom } from '../src/atom'
+import { Text, View } from 'react-native'
+
+test('useAtom should render correctly', () => {
+  const CounterAtom = atom({
+    default: 0,
+  })
+  function MyComponent(): React.ReactElement {
+    const [counter] = useAtom(CounterAtom)
+    return (
+      <View>
+        <Text>{counter}</Text>
+      </View>
+    )
+  }
+  const testRenderer = TestRenderer.create(<MyComponent />)
+  const testInstance = testRenderer.root
+
+  expect(testInstance.findByType(Text).children).toEqual(0)
+})
 
 test('explicit setState should reflect changes', () => {
   const DEFAULT_COUNTER = 0
--- tests/useSelector.test.tsx
+++ tests/useSelector.test.tsx
@@ -1,6 +1,34 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+import { renderHook, act } from '@testing-library/react-hooks/native'
 import { selector, useAtom, useSelector } from '../src'
 import { atom } from '../src/atom'
+import { Text, View } from 'react-native'
+
+test('useSelector should render correctly', () => {
+  const UserAtom = atom({
+    default: { username: 'Marc' },
+  })
+  const UsernameSelector = selector({
+    get: ({ get }) => {
+      const user = get(UserAtom)
+      return user.username
+    },
+    dependencies: [UserAtom],
+  })
+  function MyComponent(): React.ReactElement {
+    const [username] = useSelector(UsernameSelector)
+    return (
+      <View>
+        <Text>{username}</Text>
+      </View>
+    )
+  }
+  const testRenderer = TestRenderer.create(<MyComponent />)
+  const testInstance = testRenderer.root
+
+  expect(testInstance.findByType(Text).children).toEqual('Marc')
+})
 
 test('updating a dependency should update the selector', () => {
   const DEFAULT_USER = {
